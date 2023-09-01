5514ce040062f2f5d3f280b5e8f42475d0e0d19c
abusayed0 <abusayed48876@gmail.com>
2023-09-01 07:00:17 +0600

    show spinner when data is loading

--- js/phones.js
+++ js/phones.js
@@ -1,6 +1,8 @@
 console.log("js/phones.js");
 
 const loadPhones = async(phoneName) =>{
+
+   toggleHidden("loading-container", true);
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
     const res = await fetch(url);
     const data = await res.json();
@@ -9,20 +11,21 @@ const loadPhones = async(phoneName) =>{
 };
 
 const displayPhones = (phonesData) => {
+
     const phonesContainer = document.getElementById("phones-container");
     phonesContainer.innerHTML = "";
     let showPhones = phonesData;
     // display first 10 phone
-    const showAllBtn = document.getElementById("show-btn-container");
     if(phonesData.length > 12){
         console.log(showPhones.length);
         showPhones = phonesData.slice(0, 12);
-        showAllBtn.classList.remove("hidden");
+        toggleHidden("show-btn-container", true);
     }
     else{
-        showAllBtn.classList.add("hidden");
+        toggleHidden("show-btn-container", false);
     }
-   
+    toggleHidden("loading-container", false);
+
         showPhones.forEach(phoneData => {
         const {phone_name, image, brand} = phoneData;
         const newDiv = document.createElement("div");
@@ -48,4 +51,13 @@ const handleSearch = () => {
     loadPhones(searchPhone);
 }
 
+const toggleHidden = (elementId, remove) => {
+    const element = document.getElementById(elementId);
+    if(remove){
+        element.classList.remove("hidden");
+    }
+    else{
+        element.classList.add("hidden");
+    }
+};
 loadPhones("iphone");
