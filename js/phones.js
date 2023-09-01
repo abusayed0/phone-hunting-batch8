2fed75e7bbe1ba5fb8b6ce86851d222197425868
abusayed0 <abusayed48876@gmail.com>
2023-09-01 11:49:45 +0600

    added modal for every phone details

--- js/phones.js
+++ js/phones.js
@@ -29,7 +29,7 @@ const displayPhones = (phonesData, showAll) => {
     toggleHidden("loading-container", false);
 
         showPhones.forEach(phoneData => {
-        const {phone_name, image, brand} = phoneData;
+        const {phone_name, image, brand, slug} = phoneData;
         const newDiv = document.createElement("div");
         newDiv.classList.add("card", "bg-gray-100", "shadow-xl");
         newDiv.innerHTML = `
@@ -38,7 +38,7 @@ const displayPhones = (phonesData, showAll) => {
             <h2 class="card-title">${phone_name}</h2>
             <p>Brand : ${brand}</p>
             <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+                <button class="btn btn-primary" onclick="loadPhoneDetails('${slug}')">Show Details</button>
             </div>
         </div>
         `;
@@ -66,4 +66,31 @@ const toggleHidden = (elementId, remove) => {
 const handleShowAll = () => {
     loadPhones(presentSearch, true);
 }
+
+const loadPhoneDetails = async(phoneId) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showPhoneDetails(data.data);
+};
+const showPhoneDetails = (phoneData) => {
+    console.log(phoneData);
+    const {name,brand, image, mainFeatures : {storage, memory, displaySize, chipSet}} = phoneData;
+    const phoneDetailsModalBody = document.getElementById("phone-details-modal-body");
+    phoneDetailsModalBody.innerHTML = `
+  
+    <figure>
+        <img src="${image}" alt="phone" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">${name}</h2>
+      <p>Brand : ${brand}</p>
+      <p>Storage : ${storage}</p>
+      <p>Memory : ${memory}</p>
+      <p>Display size : ${displaySize}</p>
+      <p>Chip set : ${chipSet}</p>
+    </div>
+    `;
+    my_modal.showModal()
+};
 loadPhones("iphone", false);
