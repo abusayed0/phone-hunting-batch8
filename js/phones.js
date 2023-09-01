09b87ae10d6de4cec5c86d919ede68c01567d770
abusayed0 <abusayed48876@gmail.com>
2023-09-01 12:36:13 +0600

    handle fetch error

--- js/phones.js
+++ js/phones.js
@@ -2,14 +2,19 @@ console.log("js/phones.js");
 
 let presentSearch;
 
-const loadPhones = async(phoneName, showAll) =>{
+const loadPhones = async (phoneName, showAll) => {
     presentSearch = phoneName;
-   toggleHidden("loading-container", true);
+    toggleHidden("loading-container", true);
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    const phones = data.data;
-    displayPhones(phones, showAll);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        const phones = data.data;
+        displayPhones(phones, showAll);
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 const displayPhones = (phonesData, showAll) => {
@@ -18,18 +23,18 @@ const displayPhones = (phonesData, showAll) => {
     phonesContainer.innerHTML = "";
     let showPhones = phonesData;
     // display first 10 phone
-    if(phonesData.length > 12 && !showAll){
+    if (phonesData.length > 12 && !showAll) {
         showPhones = phonesData.slice(0, 12);
         toggleHidden("show-btn-container", true);
     }
-    else{
+    else {
         toggleHidden("show-btn-container", false);
     }
 
     toggleHidden("loading-container", false);
 
-        showPhones.forEach(phoneData => {
-        const {phone_name, image, brand, slug} = phoneData;
+    showPhones.forEach(phoneData => {
+        const { phone_name, image, brand, slug } = phoneData;
         const newDiv = document.createElement("div");
         newDiv.classList.add("card", "bg-gray-100", "shadow-xl");
         newDiv.innerHTML = `
@@ -44,7 +49,7 @@ const displayPhones = (phonesData, showAll) => {
         `;
         phonesContainer.appendChild(newDiv);
     })
-   
+
 };
 
 const handleSearch = () => {
@@ -55,10 +60,10 @@ const handleSearch = () => {
 
 const toggleHidden = (elementId, remove) => {
     const element = document.getElementById(elementId);
-    if(remove){
+    if (remove) {
         element.classList.remove("hidden");
     }
-    else{
+    else {
         element.classList.add("hidden");
     }
 };
@@ -67,15 +72,19 @@ const handleShowAll = () => {
     loadPhones(presentSearch, true);
 }
 
-const loadPhoneDetails = async(phoneId) => {
+const loadPhoneDetails = async (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    showPhoneDetails(data.data);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        showPhoneDetails(data.data);
+    }
+    catch(error){
+        console.log(error);
+    }
 };
 const showPhoneDetails = (phoneData) => {
-    console.log(phoneData);
-    const {name,brand, image, mainFeatures : {storage, memory, displaySize, chipSet}} = phoneData;
+    const { name, brand, image, mainFeatures } = phoneData;
     const phoneDetailsModalBody = document.getElementById("phone-details-modal-body");
     phoneDetailsModalBody.innerHTML = `
   
@@ -85,12 +94,14 @@ const showPhoneDetails = (phoneData) => {
     <div class="card-body">
       <h2 class="card-title">${name}</h2>
       <p>Brand : ${brand}</p>
-      <p>Storage : ${storage}</p>
-      <p>Memory : ${memory}</p>
-      <p>Display size : ${displaySize}</p>
-      <p>Chip set : ${chipSet}</p>
+      <p>Storage : ${mainFeatures?.storage ?? "No data found."}</p>
+      <p>Memory : ${mainFeatures?.memory ?? "No data found."}</p>
+      <p>Display size : ${mainFeatures?.displaySize ?? "No data found."}</p>
+      <p>Chip set : ${mainFeatures?.chipSet ?? "No data found."}</p>
     </div>
     `;
     my_modal.showModal()
 };
 loadPhones("iphone", false);
+
+
